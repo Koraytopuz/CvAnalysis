346394b45c0fbecb10a94cff586fd69e6db5eb7f
Koray Topuz <krytopuz@gmail.com>
2025-07-10 01:59:08 +0300

    .

--- backend/obj/Release/net8.0/CvAnalysis.Server.AssemblyInfo.cs
+++ backend/obj/Release/net8.0/CvAnalysis.Server.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("CvAnalysis.Server")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2806d7d5655cc2cce76c60249f89e816aa609045")]
 [assembly: System.Reflection.AssemblyProductAttribute("CvAnalysis.Server")]
